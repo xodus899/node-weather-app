0086cef26abd246e8d3556ab8f9eafcaa495ea34
Christopher Justice <justcd@mail.broward.edu>
2018-04-05 15:38:37 -0400

    h key

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function(req,res) {
 app.post('/', function(req,res) {
   let city = req.body.city;
   let apiEnv = process.env.apiKey;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiEnv}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiEnvHeroku}`;
   request(url, function(err,response,body) {
     if(err) {
       res.render('index', {weather: null, error: 'Error, please try again'});
